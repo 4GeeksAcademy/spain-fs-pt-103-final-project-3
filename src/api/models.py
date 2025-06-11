d8650d86e48786cb90e71423a28ee112d5097211
Juandomz91 <juandomz87@gmail.com>
2025-06-11 18:00:47 +0000

    user y recipe

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    # Relación uno-a-muchos con Recipes
+    recipes: Mapped[list["Recipe"]] = relationship(back_populates="user")
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+class Recipe(db.Model):
+    __tablename__ = "recipe"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ingredient: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="recipes")
