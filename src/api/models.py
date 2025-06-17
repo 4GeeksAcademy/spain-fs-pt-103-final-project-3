2029b2d69969d43b10538756e3c7898f6f998dd6
Juandomz91 <juandomz87@gmail.com>
2025-06-17 15:25:24 +0000

    recipe expandido

--- src/api/models.py
+++ src/api/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True, nullable=False)
 
     # Relación uno-a-muchos con Recipes
     recipes: Mapped[list["Recipe"]] = relationship(back_populates="user")
@@ -20,7 +20,10 @@ class Recipe(db.Model):
     __tablename__ = "recipe"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    ingredient: Mapped[str] = mapped_column(String(255), nullable=False)
-
+    ingredients: Mapped[str] = mapped_column(String(255), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    instructions: Mapped[str] = mapped_column(Text, nullable=False)
+    cook_time: Mapped[int] = mapped_column(nullable=True)
+    image_url: Mapped[str] = mapped_column(String(255), nullable=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     user: Mapped["User"] = relationship(back_populates="recipes")
